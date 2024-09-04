b84ce593312aa8267ad6daea0865c287abeb6b50
ankitapani15 <ankitapani15@gmail.com>
2024-09-04 23:14:57 +0530

    hello

---
+++ app.js
@@ -0,0 +1,7 @@
+const heading = React.createElement("h1",{},["hellooo",React.createElement("h1",{},'yess')])
+console.log(heading)
+const root = ReactDOM.createRoot(document.getElementById("root"))
+console.log('rrot',root)
+
+  root.render(heading)
+ 
